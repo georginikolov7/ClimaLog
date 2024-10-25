f90a6a4c8b653aa5e9fc42443e0380e567f399b6
Georgi Nikolov <gnnikoll@gmail.com>
2024-10-25 17:42:35 +0300

    MIgrated to .net8 and code cleanup

--- ClimaLog_App_MAUI/TestProject1/obj/Debug/net7.0/TestProject1.AssemblyInfo.cs
+++ ClimaLog_App_MAUI/TestProject1/obj/Debug/net7.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871d06bff503e232baf551168782eac717681fcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
